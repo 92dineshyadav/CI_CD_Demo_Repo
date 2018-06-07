8db16dcc07f43d4d6aae554440a47cb6b5a779a5
92dineshyadav <39827365+92dineshyadav@users.noreply.github.com>
2018-06-07 16:11:12 +0530

    Test
    test

--- gs-spring-boot/complete/src/main/java/hello/HelloController.java
+++ gs-spring-boot/complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Updated message!";
+        return "Updated message abc123!";
     }
     
 }
