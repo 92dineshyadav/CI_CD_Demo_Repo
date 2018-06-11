bb571a8572983ccaa19b408ac34db633cab47667
Ubuntu <dev@JenkinsVM.0stecochssxuroq0joinvubaya.cx.internal.cloudapp.net>
2018-06-11 09:12:42 +0000

    deleted

--- gs-spring-boot/CICD/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application upload --path CICD --show-progress
-sfctl application provision --application-type-build-path CICD
-sfctl application create --app-name fabric:/CICD --app-type CICDType --app-version 1.0.0
--- gs-spring-boot/CICD/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application delete --application-id CICD
-sfctl application unprovision --application-type-name CICDType --application-type-version 1.0.0
-sfctl store delete --content-path CICD
